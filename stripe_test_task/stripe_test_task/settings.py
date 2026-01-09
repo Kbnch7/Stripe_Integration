6871be0814aba843600c8eca25ac4d94ef3b706e
Артем <artem.bond2017@mail.ru>
2026-01-09 03:30:03 +0300

    add debug switcher by .env file

--- stripe_test_task/stripe_test_task/settings.py
+++ stripe_test_task/stripe_test_task/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = bool(os.getenv('django_secret_key', True))
+SECRET_KEY = os.getenv('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = [
     '*'
